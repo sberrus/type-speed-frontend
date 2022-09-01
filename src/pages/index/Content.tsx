d616882e92019828e3bdb06831e0f9db630ded2f
samdev <samuelberrus@gmail.com>
2022-09-02 00:13:44 +0200

    ...

--- src/pages/index/Content.tsx
+++ src/pages/index/Content.tsx
@@ -3,15 +3,15 @@ import { Col, Container, Row } from "react-bootstrap";
 // components
 import { TextDecoratorPrimary, TextDecoratorSecondary } from "@components/Decorators/CustomText";
 // assets
-import Banner from "@assets/img/Banner.jpg";
-import Decorator from "@assets/img/Decorator.svg";
-import Trophie from "@assets/icons/Trophie.svg";
-import Register from "@assets/icons/Register.svg";
-import Pizza from "@assets/icons/Pizza.svg";
+import Banner from "@assets/img/banner.jpg";
+import Decorator from "@assets/img/decorator.svg";
+import Trophie from "@assets/icons/trophie.svg";
+import Register from "@assets/icons/register.svg";
+import Pizza from "@assets/icons/pizza.svg";
 //
-import Ppm from "@assets/decorators/Ppm.svg";
-import Lps from "@assets/decorators/Lps.svg";
-import Acc from "@assets/decorators/Acc.svg";
+import Ppm from "@assets/decorators/ppm.svg";
+import Lps from "@assets/decorators/lps.svg";
+import Acc from "@assets/decorators/acc.svg";
 // styles
 import style from "./Index.module.scss";
 
